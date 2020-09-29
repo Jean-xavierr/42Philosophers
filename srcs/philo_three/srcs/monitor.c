b5943dfa0b8d219e2d835b9d39e2b8de8514a7c4
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-09-29 14:53:14 +0200

    :bug: fix : error with a number of meals eat (philo_three)

--- srcs/philo_three/srcs/monitor.c
+++ srcs/philo_three/srcs/monitor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   monitor.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Jeanxavier <Jeanxavier@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 15:40:13 by Jeanxavier        #+#    #+#             */
-/*   Updated: 2020/09/26 22:43:57 by Jeanxavier       ###   ########.fr       */
+/*   Updated: 2020/09/29 14:43:25 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,46 @@ void		kill_philosophers(t_data *data, t_philo *philo)
 		kill(philo[i++].pid, SIGKILL);
 }
 
+int			check_status(t_data *data, t_philo *philo, unsigned int *n_philo,
+unsigned int i)
+{
+	int					status;
+
+	status = 0;
+	waitpid(philo[i].pid, &status, WNOHANG);
+	if (WIFEXITED(status))
+	{
+		if (WEXITSTATUS(status) == 42)
+			return (42);
+		else if (WEXITSTATUS(status) == 2)
+		{
+			*n_philo = *n_philo + 1;
+			if (*n_philo == data->n_philo)
+				return (2);
+		}
+	}
+	return (0);
+}
+
 void		monitor_die(t_data *data, t_philo *philo)
 {
 	unsigned int		i;
+	unsigned int		n_philo;
 	int					status;
 
 	i = 0;
+	n_philo = 0;
 	while (i < data->n_philo)
 	{
-		waitpid(philo[i].pid, &status, WNOHANG);
-		if (WIFEXITED(status))
-		{
-			if (WEXITSTATUS(status) == 42 || WEXITSTATUS(status) == 2)
-				break ;
-		}
+		status = check_status(data, philo, &n_philo, i);
+		if (status == 42 || status == 2)
+			break ;
 		i++;
-		if (i == data->n_philo - 1)
+		if (i == data->n_philo)
 			i = 0;
 	}
 	kill_philosophers(data, philo);
-	if (WEXITSTATUS(status) == 2)
+	if (status == 2)
 		display_all_meals_ate(data);
 }
 
